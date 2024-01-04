50a4f3aee62a473b82f2f3a931b81a61832d98eb
srdobolo <149976201+srdobolo@users.noreply.github.com>
2024-01-04 00:03:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.header(":bar_chart: Recruitment Dashboard")
 # Upload CSV File
 @st.cache_data
 def load_data(file):
-    df_data = pd.read_csv(file, encoding="utf8"))
+    df_data = pd.read_csv(file, encoding="utf8")
     return df_data
 df_data = st.file_uploader('')
 if df_data is None:
